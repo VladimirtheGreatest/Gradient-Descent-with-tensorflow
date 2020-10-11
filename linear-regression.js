45aab155fda107e94ffdee2b730cb15c96449b09
Vladimir <vladkodoktorov@gmail.com>
2020-10-11 17:02:03 +0100

    calculating slopes for gradient descent

--- linear-regression.js
+++ linear-regression.js
@@ -1,13 +1,52 @@
-const tf = require('@tensorflow/tfjs');
+const tf = require("@tensorflow/tfjs");
+const _ = require("lodash");
 
 class LinearRegression {
- constructor(features, labels, options){
-   this.features = features;
-   this.labels = labels;
-        //if we dont provide learning rate in options the default rate will be 0.1
-   this.options = Object.assign({ learningRate: 0.1}, options);
+  constructor(features, labels, options) {
+    this.features = features;
+    this.labels = labels;
+    //if we dont provide learning rate in options the default rate will be 0.1
+    this.options = Object.assign(
+      { learningRate: 0.1, iterations: 1000 },
+      options
+    );
 
- }
+    //initial guesses
+    this.m = 0;
+    this.b = 0;
+  }
+
+  gradientDescent() {
+    const currentGuessesForMPG = this.features.map((row) => {
+      //inner array calculation
+      return this.m * row[0] + this.b;
+    });
+
+    //formula   sum of ((m * feature) + b) - label(actual value)
+    const bSlope =
+      (_.sum(
+        currentGuessesForMPG.map((guess, i) => {
+          return guess - this.labels[i][0];
+        })
+      ) *
+        2) /
+      this.features.length; //  * derivative / number of observation can be either length of features or labels
+
+    const mSlope =
+      (_.sum(
+        currentGuessesForMPG.map((guess, i) => {
+          return -1 * this.features[i][0] * (this.labels[i][0] - guess);
+        })
+      ) *
+        2) /
+      this.features.length;
+  }
+  
+  train() {
+    for (let index = 0; index < this.options.iterations; index++) {
+      this.gradientDescent();
+    }
+  }
 }
 
 module.exports = LinearRegression;
